98370dd64d731d560879a4e4be5dd9885e4c5250
Raul Bautista <rbmolina27@gmail.com>
2022-04-15 11:17:45 +0200

    view finished

--- WeatherApp/WeatherApp/View/ViewController.swift
+++ WeatherApp/WeatherApp/View/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonConsult: UIButton!
     let weatherConsultModel = WeatherConsult()
     
-    required init? (coder: NSCoder) {
+    /*required init? (coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
+    }*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +36,7 @@ extension ViewController {
         buttonConsult.backgroundColor = UIColor(red: 0.98, green: 0.82, blue: 0.28, alpha: 1.00)
         buttonConsult.layer.cornerRadius = 10
         weatherConsultModel.completionLabel = { temp in
-            infoWeather.text = temp
+            self.infoWeather.text = temp
         }
         
     }
