ba127809b125f92addedad98f195560869fd02b8
Raul Bautista <rbmolina27@gmail.com>
2022-04-27 20:06:56 +0200

    clean code

--- WeatherApp/WeatherApp/View/ViewController.swift
+++ WeatherApp/WeatherApp/View/ViewController.swift
@@ -12,10 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var infoWeather: UILabel!
     @IBOutlet weak var buttonConsult: UIButton!
     let weatherConsultModel = WeatherConsult()
-    
-    /*required init? (coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
